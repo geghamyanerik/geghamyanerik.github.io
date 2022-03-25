eb5aa5a0b1972e658a9d49e3513b80d915d5bdf7
geghamyanerik <arm.gamer.2006@mail.ru>
2022-03-25 21:14:58 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let svg = document.getElementById("svg")
+
+svg.addEventListener('click',function(){
+    let li = document.getElementById("list")
+  
+    if( li.style.display = "none"){
+        li.style.display = "block"
+        svg.style.display = "none"
+    }
+  
+    else{
+ 
+    }
+    
+   
+
+})
+let lock = document.querySelector(".lock")
+
+lock.addEventListener('click',function(){
+    let li = document.getElementById("list")
+    if( li.style.display = "block"){
+        li.style.display = "none"
+        svg.style.display = "block"
+    }
+}
+)
+    
